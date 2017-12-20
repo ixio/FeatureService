edecae5b3efa3ac265ee0ed45f7960bf106f069e
Alex2242 <25385645+Alex2242@users.noreply.github.com>
2017-12-20 21:18:56 +0100

    Eslint added to tests (#2)
    * Add Eslint, to be run with tests

--- test/index.js
+++ test/index.js
@@ -9,3 +9,15 @@
 require('mocha-jshint')();
 // Run jscs as part of normal testing
 require('mocha-jscs')();
+// Run eslint as part of normal testing
+require('mocha-eslint')(
+    ['sys','lib','test','projects'],
+    {
+        formatter: 'compact',
+        alwaysWarn: false,
+        timeout: 1000,
+        slow: 200,
+        strict: true,
+        contextName: 'eslint'
+    }
+);
