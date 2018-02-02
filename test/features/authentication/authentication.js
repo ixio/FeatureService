e7f0e40bfc8ed91ed5271cd934cb2c96978efb62
Joseph Allemandou <joseph.allemandou@gmail.com>
2018-02-02 10:36:42 +0100

    Update auth-related code
     - Rename from authentification to authentication
     - Add tests to lib/AuthFromHTPasswdFile.js and make
       initialization synchronous
     - Minor renaming of variables

--- test/features/authentication/authentication.js
+++ test/features/authentication/authentication.js
@@ -10,13 +10,13 @@ var preq   = require('preq');
 var server = require('../../utils/server.js');
 
 
-describe('authentification-related endpoints', function () {
+describe('authentication-related endpoints', function () {
     this.timeout(20000);
 
     //Start server before running tests
     before(function () { return server.start(); });
 
-    var endpointAuthenticate = '/authentification/authenticate';
+    var endpointAuthenticate = '/authentication/authenticate';
 
     it('should return 200 with a none empty string', function () {
         return preq.post({
@@ -34,7 +34,7 @@ describe('authentification-related endpoints', function () {
             uri: server.config.fsURL + endpointAuthenticate,
             headers: { 'content-type': 'multipart/form-data'},
             body: { username: 'alogin', password: 'password' }
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 401);
@@ -46,19 +46,19 @@ describe('authentification-related endpoints', function () {
             uri: server.config.fsURL + endpointAuthenticate,
             headers: { 'content-type': 'multipart/form-data'},
             body: { username: 'login', password: 'apassword' }
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 401);
         });
     });
 
-    var endpointToken = '/authentification/verify-token';
+    var endpointToken = '/authentication/verify-token';
 
     it('should return 401 without token', function () {
         return preq.get({
             uri: server.config.fsURL + endpointToken
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 401);
@@ -71,7 +71,7 @@ describe('authentification-related endpoints', function () {
             headers: {
               authorization: 'Bearer WrongBearer'
             }
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 401);
@@ -95,56 +95,5 @@ describe('authentification-related endpoints', function () {
             });
         });
     });
-
-    var endpointAuthTS = '/examples/authentified-fake-timeserie/2017-01-01/2017-02-01/day';
-
-    it('should return 401 without token', function () {
-        return preq.get({
-            uri: server.config.fsURL + endpointAuthTS
-        }).then(function(res) {
-          throw 'Should not succeed'
-        }).catch(function(res) {
-            assert.deepEqual(res.status, 401);
-        });
-    });
-
-    it('should return 401 with a wrong token', function () {
-        return preq.get({
-            uri: server.config.fsURL + endpointAuthTS,
-            headers: {
-              authorization: 'Bearer WrongBearer'
-            }
-        }).then(function(res) {
-          throw 'Should not succeed'
-        }).catch(function(res) {
-            assert.deepEqual(res.status, 401);
-        });
-    });
-
-    it('should return 200 with a correct token', function () {
-        return preq.post({
-            uri: server.config.fsURL + endpointAuthenticate,
-            headers: { 'content-type': 'multipart/form-data'},
-            body: { username: 'login', password: 'password' }
-        }).then(function(res) {
-            assert.deepEqual(res.status, 200);
-            preq.get({
-                uri: server.config.fsURL + endpointAuthTS,
-                headers: {
-                  authorization: 'Bearer ' + res.body.token
-                }
-            }).then(function(res) {
-                assert.deepEqual(res.status, 200);
-                assert.deepEqual(res.status, 200);
-                assert.deepEqual(res.body.items.length, 31);
-                // checking first and last timestamps
-                assert.deepStrictEqual(res.body.items[0].ts, '2017-01-01T00:00:00.000Z');
-                assert.deepStrictEqual(res.body.items[30].ts, '2017-01-31T00:00:00.000Z');
-            });
-        });
-    });
-
-
-
 
 });
--- test/features/examples/examples.js
+++ test/features/examples/examples.js
@@ -22,7 +22,7 @@ describe('examples endpoints', function () {
     it('should return 400 when fake-timeserie parameters are wrong', function () {
         return preq.get({
             uri: server.config.fsURL + endpoint + '/2017-wrong-01/2017-02-01/day'
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 400);
@@ -32,7 +32,7 @@ describe('examples endpoints', function () {
     it('should return 400 when to is before from', function () {
         return preq.get({
             uri: server.config.fsURL + endpoint + '/2017-03-01/2017-02-01/day'
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 400);
@@ -42,7 +42,7 @@ describe('examples endpoints', function () {
     it('should return 400 when date-time is invalid', function () {
         return preq.get({
             uri: server.config.fsURL + endpoint + '/2017-01-01/2017-02-35/day'
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 400);
@@ -103,7 +103,7 @@ describe('examples endpoints', function () {
     it('should return 400 when to is before from', function () {
         return preq.get({
             uri: server.config.fsURL + endpointMean + '/2017-03-01/2017-02-01/day'
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 400);
@@ -113,7 +113,7 @@ describe('examples endpoints', function () {
     it('should return 400 when date-time is invalid', function () {
         return preq.get({
             uri: server.config.fsURL + endpointMean + '/2017-01-01/2017-02-35/day'
-        }).then(function(res) {
+        }).then(function() {
           throw 'Should not succeed'
         }).catch(function(res) {
             assert.deepEqual(res.status, 400);
@@ -175,6 +175,56 @@ describe('examples endpoints', function () {
             assert.deepStrictEqual(res.body.items[0].length, 60);
             assert.deepStrictEqual(typeof(res.body.items[0].mean), 'number');
             assert.deepStrictEqual(0 < res.body.items[0].mean && res.body.items[0].mean < 1, true);
+        });
+    });
+
+
+    var endpointAuthenticate = '/authentication/authenticate';
+    var endpointAuthTS = '/examples/authentified-fake-timeserie/2017-01-01/2017-02-01/day';
+
+    it('should return 401 without token', function () {
+        return preq.get({
+            uri: server.config.fsURL + endpointAuthTS
+        }).then(function() {
+          throw 'Should not succeed'
+        }).catch(function(res) {
+            assert.deepEqual(res.status, 401);
+        });
+    });
+
+    it('should return 401 with a wrong token', function () {
+        return preq.get({
+            uri: server.config.fsURL + endpointAuthTS,
+            headers: {
+              authorization: 'Bearer WrongBearer'
+            }
+        }).then(function() {
+          throw 'Should not succeed'
+        }).catch(function(res) {
+            assert.deepEqual(res.status, 401);
+        });
+    });
+
+    it('should return 200 with a correct token', function () {
+        return preq.post({
+            uri: server.config.fsURL + endpointAuthenticate,
+            headers: { 'content-type': 'multipart/form-data'},
+            body: { username: 'login', password: 'password' }
+        }).then(function(res) {
+            assert.deepEqual(res.status, 200);
+            preq.get({
+                uri: server.config.fsURL + endpointAuthTS,
+                headers: {
+                  authorization: 'Bearer ' + res.body.token
+                }
+            }).then(function(res) {
+                assert.deepEqual(res.status, 200);
+                assert.deepEqual(res.status, 200);
+                assert.deepEqual(res.body.items.length, 31);
+                // checking first and last timestamps
+                assert.deepStrictEqual(res.body.items[0].ts, '2017-01-01T00:00:00.000Z');
+                assert.deepStrictEqual(res.body.items[30].ts, '2017-01-31T00:00:00.000Z');
+            });
         });
     });
 
