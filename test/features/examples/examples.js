2cb9049335aa7223d92525f7a9969055e947c3dc
Joseph Allemandou <joseph.allemandou@gmail.com>
2018-06-08 11:02:28 +0200

    Update copyright to include 2018 (#15)

--- test/features/examples/examples.js
+++ test/features/examples/examples.js
@@ -1,4 +1,24 @@
+/* Copyright (C) 2017-2018 Project-ODE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /*
+ * Test utility faking a ES cluster.
+ * Check received query against known ones using fixtures files
+ * and sends data or error back.
+ *
  * ODE-FeatureService Examples tests
  * Author: Joseph Allemandou
  */
