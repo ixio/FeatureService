44215147152788646d9c7f71d3e094565856c624
ixio <erwan.keribin@gmail.com>
2018-07-25 10:11:49 +0200

    Add dataset endpoint (#18)

---
+++ sys/dataset.js
@@ -0,0 +1,68 @@
+/* Copyright (C) 2017-2018 Project-ODE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ODE-FeatureService dataset-related functions
+ * Author: Erwan Keribin
+ */
+'use strict';
+var HyperSwitch = require('hyperswitch');
+var path = require('path');
+var fsUtil = require('../lib/FeatureServiceUtil');
+
+var spec = HyperSwitch.utils.loadSpec(path.join(__dirname, 'dataset.yaml'));
+
+const db = require('../db');
+
+class Dataset {
+    constructor(options) {
+        this.options = options;
+    }
+
+    list() {
+        return db.Dataset.query()
+        .select(
+            'id',
+            'name',
+            'files_type',
+            'start_date',
+            'end_date',
+            db.Dataset.relatedQuery('dataset_type').select('name').as('type'),
+            db.Dataset.relatedQuery('files').count().as('files_count')
+        ).then(datasets => {
+            for (let dataset of datasets) {
+                // Since PGSQL can return a bigint on count, knex will return a string
+                // cf https://knexjs.org/#Builder-count
+                dataset.files_count = parseInt(dataset.files_count);
+            }
+            return fsUtil.normalizeResponse({
+                status: 200,
+                body: datasets
+            });
+        });
+    }
+}
+
+module.exports = function(options) {
+    var dataset = new Dataset(options);
+
+    return {
+        spec: spec,
+        operations: {
+            list: dataset.list.bind(dataset)
+        }
+    };
+};
---
+++ test/features/dataset/dataset.js
@@ -0,0 +1,65 @@
+/* Copyright (C) 2017-2018 Project-ODE
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * ODE-FeatureService dataset-related api tests
+ * Author: Erwan Keribin
+ */
+'use strict';
+
+
+var assert = require('../../utils/assert.js');
+var preq   = require('preq');
+var server = require('../../utils/server.js');
+var db     = require('../../../db');
+
+
+describe('dataset endpoints', function () {
+    this.timeout(20000);
+
+    // Start server and DB before running tests
+    before(async function () {
+        await server.start();
+        await db.init();
+    });
+
+    var endpointList = '/dataset/list';
+
+    it('should return 200 with the list of datasets', function () {
+        return preq.get({
+            uri: server.config.fsURL + endpointList
+        }).then(function(res) {
+            assert.deepEqual(res.status, 200);
+            // checking we have the right number of datasets
+            assert.deepEqual(res.body.length, 3);
+            // checking first dataset info
+            var dataset = res.body[0];
+            assert.deepStrictEqual(dataset.id, 1);
+            assert.deepStrictEqual(dataset.name, 'SPMAuralA2010');
+            assert.deepStrictEqual(dataset.files_type, '.wav');
+            assert.deepStrictEqual(new Date(dataset.start_date), new Date(2010, 7, 19));
+            assert.deepStrictEqual(new Date(dataset.end_date), new Date(2010, 10, 2));
+            assert.deepStrictEqual(dataset.type, 'PAM');
+            assert.deepStrictEqual(dataset.files_count, 2);
+        });
+    });
+
+    after(function() {
+        db.close();
+        server.stop();
+    });
+
+});
